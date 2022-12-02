add744e6151dfaee5ce798fc2735e60b8e06dcde
akostothz <akostothza@gmail.com>
2022-12-02 20:29:32 +0100

    Added an Angular route guard.

--- AngularClient/src/app/app-routing.module.ts
+++ AngularClient/src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { LoggedHomeComponent } from './logged-home/logged-home.component';
 import { ForyouComponent } from './musics/foryou/foryou.component';
 import { MusicDetailComponent } from './musics/music-detail/music-detail.component';
 import { MusicListComponent } from './musics/music-list/music-list.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'musics', component: MusicListComponent},
-  {path: 'loggedHome', component: LoggedHomeComponent},
-  {path: 'musics/:id', component: MusicDetailComponent},
-  {path: 'foryou', component: ForyouComponent},
+  {path: 'musics', component: MusicListComponent, canActivate: [AuthGuard]},
+  {path: 'loggedHome', component: LoggedHomeComponent, canActivate: [AuthGuard]},
+  {path: 'musics/:id', component: MusicDetailComponent, canActivate: [AuthGuard]},
+  {path: 'foryou', component: ForyouComponent, canActivate: [AuthGuard]},
   {path: 'about', component: AboutComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'} //the invalid route
 ];
