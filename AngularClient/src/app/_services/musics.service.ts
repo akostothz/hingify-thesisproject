f5a48cd90f6938db5cb4d185a310f6ece7fc0220
akostothz <akostothza@gmail.com>
2023-01-23 21:26:00 +0100

    Created the music service for the mixes.

---
+++ AngularClient/src/app/_services/musics.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Music } from '../_models/music';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MusicsService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getPersonalizedMix(id: number) {
+    return this.http.get<Music[]>(this.baseUrl + 'GetPersonalizedMix/' + id, this.getHttpOptions());
+  }
+
+  getLikedSongs(id: number) {
+    return this.http.get<Music[]>(this.baseUrl + 'GetLikedSongs/' + id, this.getHttpOptions());
+  }
+
+  getMusicsBySex(id: number) {
+    return this.http.get<Music[]>(this.baseUrl + 'GetMusicsBySex/' + id, this.getHttpOptions());
+  }
+
+  getMusicsByCountry(id: number) {
+    return this.http.get<Music[]>(this.baseUrl + 'GetMusicsByCountry/' + id, this.getHttpOptions());
+  }
+
+  getMusicsByAgeGroup(id: number) {
+    return this.http.get<Music[]>(this.baseUrl + 'GetMusicsByAgeGroup/' + id, this.getHttpOptions());
+  }
+
+  getHttpOptions() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    return {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + user.token
+      })
+    }
+  }
+}
