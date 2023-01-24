59411bb432c2431889bc9d97126b7091388ca554
akostothz <akostothza@gmail.com>
2023-01-24 23:12:43 +0100

    Added state-storing via service.

--- AngularClient/src/app/_services/musics.service.ts
+++ AngularClient/src/app/_services/musics.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Music } from '../_models/music';
 import { User } from '../_models/user';
@@ -9,29 +10,64 @@ import { User } from '../_models/user';
 })
 export class MusicsService {
   baseUrl = environment.apiUrl;
+  likedsongs: Music[] = [];
+  gendersongs: Music[] = [];
+  bycountrysongs: Music[] = [];
+  agesongs: Music[] = [];
+  personalizedsongs: Music[] = [];
 
   constructor(private http: HttpClient) { }
 
   
 
   getLikedSongs(id: number) {
-    return this.http.get<Music[]>(this.baseUrl + 'Music/GetLikedSongs/' + id);
+    if (this.likedsongs.length > 0) return of(this.likedsongs);
+    return this.http.get<Music[]>(this.baseUrl + 'Music/GetLikedSongs/' + id).pipe(
+      map(likedsongs => {
+        this.likedsongs = this.likedsongs;
+        return likedsongs;
+      })
+    );
   }
 
   getMusicsBySex(id: number) {
-    return this.http.get<Music[]>(this.baseUrl + 'Music/GetMusicsBySex/' + id);
+    if (this.gendersongs.length > 0) return of(this.gendersongs);
+    return this.http.get<Music[]>(this.baseUrl + 'Music/GetMusicsBySex/' + id).pipe(
+      map(gendersongs => {
+        this.gendersongs = this.gendersongs;
+        return gendersongs;
+      })
+    );
   }
 
   getMusicsByCountry(id: number) {
-    return this.http.get<Music[]>(this.baseUrl + 'Music/GetMusicsByCountry/' + id);
+    if (this.bycountrysongs.length > 0) return of(this.bycountrysongs);
+    return this.http.get<Music[]>(this.baseUrl + 'Music/GetMusicsByCountry/' + id).pipe(
+      map(bycountrysongs => {
+        this.bycountrysongs = this.bycountrysongs;
+        return bycountrysongs;
+      })
+    );
   }
 
   getMusicsByAgeGroup(id: number) {
-    return this.http.get<Music[]>(this.baseUrl + 'Music/GetMusicsByAgeGroup/' + id);
+    if (this.agesongs.length > 0) return of(this.agesongs);
+    return this.http.get<Music[]>(this.baseUrl + 'Music/GetMusicsByAgeGroup/' + id).pipe(
+      map(agesongs => {
+        this.agesongs = this.agesongs;
+        return agesongs;
+      })
+    );
   }
 
   getPersonalizedMix(id: number) {
-    return this.http.get<Music[]>(this.baseUrl + 'Music/GetPersonalizedMix/' + id);
+    if (this.personalizedsongs.length > 0) return of(this.personalizedsongs);
+    return this.http.get<Music[]>(this.baseUrl + 'Music/GetPersonalizedMix/' + id).pipe(
+      map(personalizedsongs => {
+        this.personalizedsongs = this.personalizedsongs;
+        return personalizedsongs;
+      })
+    );
   }
  
 }
--- AngularClient/src/app/musics/music-list/music-list.component.ts
+++ AngularClient/src/app/musics/music-list/music-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Music } from 'src/app/_models/music';
 import { MusicsService } from 'src/app/_services/musics.service';
 
@@ -8,33 +9,20 @@ import { MusicsService } from 'src/app/_services/musics.service';
   styleUrls: ['./music-list.component.css']
 })
 export class MusicListComponent implements OnInit {
-  likedsongs: Music[] = [];
-  originsongs: Music[] = [];
-  agesongs: Music[] = [];
-  gendersongs: Music[] = [];
+  likedsongs$: Observable<Music[]> | undefined;
+  originsongs$: Observable<Music[]> | undefined;
+  agesongs$: Observable<Music[]> | undefined;
+  gendersongs$: Observable<Music[]> | undefined;
   
   constructor(private musicService: MusicsService) {
 
   }
 
   ngOnInit(): void {
-    this.loadMusics();
+    this.likedsongs$ = this.musicService.getLikedSongs(JSON.parse(localStorage.getItem('user'))?.id);
+    this.originsongs$ = this.musicService.getMusicsByCountry(JSON.parse(localStorage.getItem('user'))?.id);
+    this.agesongs$ = this.musicService.getMusicsByAgeGroup(JSON.parse(localStorage.getItem('user'))?.id);
+    this.gendersongs$ = this.musicService.getMusicsBySex(JSON.parse(localStorage.getItem('user'))?.id);
   }
-  
-  loadMusics() {
-      this.musicService.getLikedSongs(JSON.parse(localStorage.getItem('user'))?.id).subscribe({
-        next: likedsongs => this.likedsongs = likedsongs
-      }),
-      this.musicService.getMusicsByCountry(JSON.parse(localStorage.getItem('user'))?.id).subscribe({
-        next: originsongs => this.originsongs = originsongs
-      }),
-      this.musicService.getMusicsByAgeGroup(JSON.parse(localStorage.getItem('user'))?.id).subscribe({
-        next: agesongs => this.agesongs = agesongs
-      }),
-      this.musicService.getMusicsBySex(JSON.parse(localStorage.getItem('user'))?.id).subscribe({
-        next: gendersongs => this.gendersongs = gendersongs
-      })
-  }
-  
  
 }
