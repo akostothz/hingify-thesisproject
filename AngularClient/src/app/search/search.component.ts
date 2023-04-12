ec2f7017906e044fa2b903a4f49ec5d41528bfa2
akostothz <akostothza@gmail.com>
2023-04-12 20:42:08 +0200

    search component styling

--- AngularClient/src/app/search/search.component.ts
+++ AngularClient/src/app/search/search.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MusicsService } from '../_services/musics.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
+  ngOnInit(): void {
+    
+  }
+
+  constructor(private musicService: MusicsService) { }
+
+  /*
+  const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+  const inputValue = searchInput.value;
+  alert(inputValue);
+});
+  */
 }
