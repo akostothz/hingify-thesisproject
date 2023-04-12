0c536b28a03be533eeb8464e845005a133c8ff5c
akostothz <akostothza@gmail.com>
2023-04-12 21:32:42 +0200

    some works on the statistics component.

--- AngularClient/src/app/search/search.component.ts
+++ AngularClient/src/app/search/search.component.ts
@@ -12,7 +12,7 @@ export class SearchComponent implements OnInit {
     
   }
 
-  constructor(private musicService: MusicsService) { }
+  constructor() { }
 
   /*
   const searchButton = document.getElementById('search-button');
