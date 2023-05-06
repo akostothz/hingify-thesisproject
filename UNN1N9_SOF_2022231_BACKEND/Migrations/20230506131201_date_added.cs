cbfdfb6c9b8fd1b15a8bb4e29f6c2f34591b502b
akostothz <akostothza@gmail.com>
2023-05-06 15:12:37 +0200

    Added DateOnly prop to behaviors.

---
+++ UNN1N9_SOF_2022231_BACKEND/Migrations/20230506131201_date_added.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UNN1N9SOF2022231BACKEND.Migrations
+{
+    /// <inheritdoc />
+    public partial class dateadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "UserBehaviors",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "UserBehaviors");
+        }
+    }
+}
